726462a58370ec02a47827b9d0b45370e4f56500
zaqxssw79218@gmail.com <Aa29066110>
2022-01-10 23:24:14 +0800

    chain of responsibility pattern

---
+++ chain of resposibility/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type human interface {
+	Age() int
+}
+
+type person struct {
+	age int
+}
+
+func (p person) Age() int {
+	return p.age
+}
+
+type tw struct {
+	person
+}
+
+func (t tw) String() string {
+	return fmt.Sprintf("ags: %v", t.age)
+}
+
+func main() {
+	a := tw{person{age: 39}}
+	i := a.Age()
+	fmt.Println(i)
+	fmt.Println(a.String())
+}
